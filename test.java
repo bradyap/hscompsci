31a3197f36cd9cdab372d5118e9f28e980d56cef
bradyap <bpettengill3@gmail.com>
2021-01-27 13:13:31 -0500

    stupid quiz man

--- test.java
+++ test.java
@@ -1,8 +1,30 @@
 public class test {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         
     }
 
+    /*public static void maxScorers(Student[] students) {
+        int temp = 0;
+        for (int i = 0; i < students.length; i++) {
+            if (students[i].getScore() > temp) {
+                temp = students[i].getScore()
+            }
+        }
+        System.out.println("The max score of " + temp + " was achieved by: ");
+        for (int i = 0; i < students.length; i++) {
+            if (students[i].getScore() == temp) {
+                System.out.print(students[i].getName() + " ");
+            }
+        }
+    }*/
+
+    /*public static int findAverageAge (int[] nums) {
+        int temp = 0;
+        for (int x : nums) {
+            temp += x;
+        }
+        return (temp / nums.length);
+    }*/
 
     /*public static String capitalizeMe(String input) {
         int index = 0;
@@ -23,6 +45,4 @@ public class test {
         String output = input.substring(ran, ran + 1);
         return output;
     }*/
-
-    //git test
 }
