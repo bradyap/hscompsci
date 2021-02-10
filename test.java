413eed22039a0eea86e7312a887b7a7e3cb5d6e3
bradyap <bpettengill3@gmail.com>
2021-02-10 13:16:22 -0500

    i hate this class

--- test.java
+++ test.java
@@ -1,7 +1,55 @@
 public class test {
     public static void main(String[] args) {
-        
+
     }
+
+    /*public void findMax(int[][] ary) {
+        int max = 0;
+        
+        for(int i = 0; i < ary.length; i++) {
+            for(int j = 0; j < ary[0].length; j++) {
+                if(ary[i][j] > max) {
+                    max = ary[i][j];
+                }
+            }
+        }
+
+        
+        
+        for(int i = 0; i < ary.length; i++) {
+            for(int j = 0; j < ary[0].length; j++) {
+                if(ary[i][j] == max){
+                    System.out.println(max + " was found in row " + (i + 1) + " and column " + (j + 1) + ".");
+                }
+            }
+        } 
+    }*/
+
+    /*public static void change(int[][] matrix) {
+        int[][] temp = matrix;
+        for (int p=1; p < temp.length; p++) {
+            for (int q=0; q < temp[0].length; q++) {
+                int swap = temp[p][q];
+                temp[p][q] = temp[p-1][q];
+                temp[p-1][q] = swap;
+            }
+        }
+    }*/
+
+    /*public static boolean checkIndexes(double[][] data, int row, int col)
+    {
+    int numRows = data.length;
+    if (row < numRows)
+    {
+        int numCols = data[0].length;
+        return col < numCols;
+    }
+    else
+        return false;
+    }*/
+
+    /*String[][] chess = new String[8][8];
+        String myPiece = chess[5][6];*/
 
     /*public static void maxScorers(Student[] students) {
         int temp = 0;
