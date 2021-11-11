a868766544cdbe07ea8c9d3985ab393278e7e4ea
bradyap <bpettengill3@gmail.com>
2021-11-10 19:21:18 -0500

    oh that was easy

--- CS B/MorseCode/MorseCodeDriver.java
+++ CS B/MorseCode/MorseCodeDriver.java
@@ -9,14 +9,14 @@ public class MorseCodeDriver {
          
       //initialize maps for morsecode & plaintext
       HashMap<String, String> morseCode = new HashMap<String, String>();
-      HashMap<String, String> plainText = new HashMap<String, String>();
+      HashMap<String, String> plaintext = new HashMap<String, String>();
 
       //read in morsecode & plaintext arrays
       for (int i = 0; i < MorseCode.english.length; i++) {
          morseCode.put(MorseCode.english[i], MorseCode.code[i]);
       }
       for (int i = 0; i < MorseCode.code.length; i++) {
-         plainText.put(MorseCode.code[i], MorseCode.english[i]);
+         plaintext.put(MorseCode.code[i], MorseCode.english[i]);
       }
       
       //user chooses to encode or decode
@@ -35,21 +35,71 @@ public class MorseCodeDriver {
          inputType = input.nextLine();
       }
 
-      input.close();
+      //if user chooses to read from file
+      if (inputType.equals("f")) {
+         System.out.println("Please enter the file name:");
+         String fileName = input.nextLine(); //get filename
+         //read from file into string
+         File file = new File(fileName);
+         Scanner fileInput = new Scanner(file);
+         while (fileInput.hasNextLine()) {
+            String line = fileInput.nextLine();
+            if (direction.equals("e")) { //if user wants to encode
+               System.out.println(encode(line, morseCode));
+            }
+            else { //if user wants to decode
+               System.out.println(decode(line, plaintext));
+            }
+         }
+         fileInput.close(); //close file scanner
+      }
+      //if user choose to use from keyboard
+      else {
+         System.out.println("Please enter the text you would like to translate:");
+         String line = input.nextLine(); //get text
+         if (direction.equals("e")) { //if user wants to encode
+            System.out.println(encode(line, morseCode));
+         }
+         else { //if user wants to decode
+            System.out.println(decode(line, plaintext));
+         }
+      }
+
+      input.close(); //close input scanner
    }
+
 
    public static String encode(String input, HashMap<String, String> morsecode) {
       String output = "";      
-      input = input.toUpperCase(); //hashmaps have letters in uppercase
 
-      for (int i = 0; i < input.length(); i++) {
-         char c = input.charAt(i); //get character from input
-         output += morsecode.get(String.valueOf(c)) + " "; //append morse code for given character to output
+      input = input.toUpperCase(); //hashmaps have letters in uppercase
+      char[] chars = input.toCharArray(); //convert string to char array
+
+      for (char c : chars) { //for each char in character in input
+         if (c == ' ') { //if character is a space
+            output += "/ "; //add space to output
+         } else { //if character is not a space
+            output += morsecode.get(String.valueOf(c)); //add morsecode to output
+            output += " "; //add space after morsecode
+         }
       }
 
-      return output;
+      return output.toLowerCase(); //return output in lowercase
    }
 
-   public static String decode(String input, HashMap<String, String> english )
+   public static String decode(String input, HashMap<String, String> plaintext) {
+      String output = "";
+
+      String[] chars = input.split(" "); //split input into morse code for plaintext characters
+
+      for (String c : chars) { //for each char in character in input
+         if (c.equals("/")) { //if character is a space
+            output += " "; //add space to output
+         } else { //if character is not a space
+            output += plaintext.get(c); //add plaintext to output
+         }
+      }
+
+      return output.toLowerCase(); //return output in lowercase
+   }
 }
-      
