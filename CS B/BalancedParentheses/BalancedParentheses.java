40903c48df0bf063463c447b2e1f98ca44d4aff1
Brady <58640929+bradyap@users.noreply.github.com>
2021-12-07 10:30:47 -0500

    parentheses

---
+++ CS B/BalancedParentheses/BalancedParentheses.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+import java.util.HashMap;
+ 
+public class BalancedParentheses {
+    static boolean checkBrackets(String input) {
+        Stack<char> stack = new Stack<char>(); //create stack
+ 
+        for (int i = 0; i < input.length(); i++) { //traverse expression
+            char c = input.charAt(i);
+            char temp;
+ 
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            }
+
+            if (stack.isEmpty()) {
+                return false;
+            }
+            
+            if (c == ')') {
+                temp = stack.pop();
+                if (temp != '(') {
+                    return false;
+                }
+            } else if (c == ']') {
+                temp = stack.pop();
+                if (temp != '[') {
+                    return false;
+                }
+            } else if (c == '}') {
+                temp = stack.pop();
+                if (temp != '{') {
+                    return false;
+                }
+            }
+        }
+        return (stack.isEmpty());
+    }
+ 
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter an expression to validate:");
+        String input = in.nextLine(); //take user input
+        if (checkBrackets(input)) { //evaluate input and return result
+            System.out.println("The expression is valid.");
+        } else {
+            System.out.println("The expression is not valid.");
+        }
+    }
+}
