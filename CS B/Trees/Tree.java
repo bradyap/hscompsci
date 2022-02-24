599303c4f2da6db674b84c819b0b49372ed29770
bradyap <bpettengill3@gmail.com>
2022-02-24 01:52:34 -0500

    asdfadsfdsaafasdfokpjiadsf;klasf'ds

--- CS B/Trees/Tree.java
+++ CS B/Trees/Tree.java
@@ -9,50 +9,51 @@ public class Tree {
    
    //pre: root points to an in-order Binary Search Tree
    //post:adds x to the tree such that the tree is still an in-order Binary Search Tree
+   
    public void add(Comparable x) {
-
       myRoot = addHelper(myRoot, x);
    }
    
    private TreeNode addHelper(TreeNode root, Comparable x) {
       if (root == null) {
-         return new TreeNode(x);
-      }
-      if (x.compareTo(root.myItem) < 0) {
-         root.myLeft = addHelper(root.myLeft, x);
-      }
-      else if (x.compareTo(root.myItem) > 0) {
-         root.myRight = addHelper(root.myRight, x);
+         root = new TreeNode(x);
+      } else if (x.compareTo(root.getValue()) < 0) {
+         root.setLeft(addHelper(root.getLeft(), x));
+      } else if (x.compareTo(root.getValue()) > 0) {
+         root.setRight(addHelper(root.getRight(), x));
       }
       return root;
    }
    
    //pre: root points to an in-order Binary Search Tree
    //post:removes x from the tree such that the tree is still an in-order Binary Search Tree
-   public void remove(Comparable x){
+   
+   public void remove(Comparable x) {
       myRoot = removeHelper(myRoot, x);
    }
 
-   private TreeNode removeHelper(TreeNode root, Comparable x) {          
+   private Comparable getLow(TreeNode root) {
+      if (root.getLeft() == null) {
+         return root.getValue();
+      } else {
+         return getLow(root.getLeft());
+      }
+   }   
+
+   private TreeNode removeHelper(TreeNode root, Comparable x) {
       if (root == null) {
          return null;
-      }
-      if (x.compareTo(root.myItem) < 0) {
-         root.myLeft = removeHelper(root.myLeft, x);
-      }
-      else if (x.compareTo(root.myItem) > 0) {
-         root.myRight = removeHelper(root.myRight, x);
-      }
-      else {
-         if (root.myLeft == null) {
-            return root.myRight;
-         }
-         else if (root.myRight == null) {
-            return root.myLeft;
-         }
-         else {
-            root.myItem = findMin(root.myRight).myItem;
-            root.myRight = removeHelper(root.myRight, root.myItem);
+      } else if (x.compareTo(root.getValue()) < 0) {
+         root.setLeft(removeHelper(root.getLeft(), x));
+      } else if (x.compareTo(root.getValue()) > 0) {
+         root.setRight(removeHelper(root.getRight(), x));
+      } else {
+         if (root.getLeft() == null) {
+            return root.getRight();
+         } else if (root.getRight() == null) {
+            return root.getLeft();
+         } else {
+            root.setValue(getLow(root.getRight()));
          }
       }
       return root;
@@ -60,6 +61,7 @@ public class Tree {
    
    //pre: root points to an in-order Binary Search Tree
    //post:shows the elements of the tree such that they are displayed in prefix order
+   
    public void showPreOrder() {
       preOrderHelper(myRoot);
       System.out.println();
@@ -67,14 +69,15 @@ public class Tree {
    
    private void preOrderHelper(TreeNode root) {
       if (root != null) {
-         System.out.print(root.myItem + " ");
-         preOrderHelper(root.myLeft);
-         preOrderHelper(root.myRight);
+         System.out.println(root.getValue() + " ");
+         preOrderHelper(root.getLeft());
+         preOrderHelper(root.getRight());
       }
    }
    
    //pre: root points to an in-order Binary Search Tree
    //post:shows the elements of the tree such that they are displayed in infix order
+   
    public void showInOrder() {
       inOrderHelper(myRoot);
       System.out.println();
@@ -90,40 +93,39 @@ public class Tree {
       
    //pre: root points to an in-order Binary Search Tree
    //post:shows the elements of the tree such that they are displayed in postfix order
-   public void showPostOrder()
-   {
+   
+   public void showPostOrder() {
       postOrderHelper(myRoot);
       System.out.println();
       
    }
-
+   
    private void postOrderHelper(TreeNode root) {
       if (root != null) {
-         postOrderHelper(root.myLeft);
-         postOrderHelper(root.myRight);
-         System.out.print(root.myItem + " ");
+         postOrderHelper(root.getLeft());
+         postOrderHelper(root.getRight());
+         System.out.println(root.getValue() + " ");
       }
    }
    
    //pre: root points to an in-order Binary Search Tree
    //post:returns whether or not x is found in the tree
+   
    public boolean contains(Comparable x) {
-      if (searchHelper(myRoot, x)==null)
+      if (searchHelper(myRoot, x) == null) {
          return false;
+      }
       return true;
    }
    
    private TreeNode searchHelper(TreeNode root, Comparable x) {
       if (root == null) {
          return null;
-      }
-      if (x.compareTo(root.myItem) < 0) {
-         return searchHelper(root.myLeft, x);
-      }
-      else if (x.compareTo(root.myItem) > 0) {
-         return searchHelper(root.myRight, x);
-      }
-      else {
+      } else if (x.compareTo(root.getValue()) < 0) {
+         return searchHelper(root.getLeft(), x);
+      } else if (x.compareTo(root.getValue()) > 0) {
+         return searchHelper(root.getRight(), x);
+      } else {
          return root;
       }
    }
@@ -131,31 +133,47 @@ public class Tree {
    //pre: root points to an in-order Binary Search Tree
    //post:returns a reference to the parent of the node that contains x, returns null if no such node exists
    //THIS WILL BE CALLED IN THE METHOD removeRecur
-   private TreeNode searchParent(TreeNode root, Comparable x) { 
-      return null;
+   private TreeNode searchParent(TreeNode root, Comparable x) {
+      TreeNode p = root;
+
+      if (root == null) {
+         return null;
+      } else if (x.compareTo(root.getValue()) < 0) {
+         p = searchParent(root.getLeft(), x);
+      } else if (x.compareTo(root.getValue()) > 0) {
+         p = searchParent(root.getRight(), x);
+      }
+
+      return p;
    }
    
    //post: determines if root is a leaf or not O(1)
-   private boolean isLeaf(TreeNode root) { 
-      return (root.myLeft == null && root.myRight == null);
+   private boolean isLeaf(TreeNode root) {
+      if (root.getLeft() == null && root.getRight() == null) {
+         return true;
+      }
+
+      return false;
    }
       
    //post: returns true if only one child O(1)
    private boolean oneKid(TreeNode root) {
-      return (root.myLeft != null && root.myRight == null) || (root.myLeft == null && root.myRight != null);
+      
+      return false;
    }
       
    //pre: root points to an in-order Binary Search Tree
    //post:returns the number of nodes in the tree
+   
    public int size() {
       return sizeHelper(myRoot);
    }
    
    private int sizeHelper(TreeNode root) {
-      if (root == null) {
-         return 0;
-      }
-      return 1 + sizeHelper(root.myLeft) + sizeHelper(root.myRight);
+   //************COMPLETE THIS METHOD*****************************
+   
+   //************************************************************  
+      return 0;
    }
          
    public int height() {
@@ -166,10 +184,10 @@ public class Tree {
    //post:returns the height (depth) of the tree
    
    public int heightHelper(TreeNode root) {
-      if (root == null) {
-         return 0;
-      }
-      return 1 + Math.max(heightHelper(root.myLeft), heightHelper(root.myRight));
+   //************COMPLETE THIS METHOD*****************************
+   
+   //************************************************************  
+      return 0;
    }
    
    //EXTRA CREDIT
@@ -183,15 +201,15 @@ public class Tree {
    //EXTRA CREDIT
    //pre: root points to an in-order Binary Search Tree
    //post:shows all elements of the tree at a particular depth
+   
    public void printLevel(TreeNode root, int level) {
       
    }
 
   //Nothing to see here...move along.
    private String temp;
-   private void inOrderHelper2(TreeNode root)   {
-      if(root!=null)
-      {
+   private void inOrderHelper2(TreeNode root) {
+      if(root!=null) {
          inOrderHelper2(root.getLeft());
          temp += (root.getValue() + ", "); 
          inOrderHelper2(root.getRight());
